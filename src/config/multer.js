ff30ab4995806bcbbf074b1982b18668823f3759
Felipe Barros <felipetech@tuta.io>
2020-02-07 12:06:59 -0300

    Refatorando arquivo de configuração do multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,29 +1,32 @@
-const multer = require('multer')
 const { resolve } = require('path')
+const multer = require('multer')
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination (req, file, cb) {
     cb(null, resolve(__dirname, '..', '..', 'uploads'))
   },
-  filename: function (req, file, cb) {
-    cb(null, new Date().toISOString() + file.originalname)
+  filename (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`)
   }
 })
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-    cb(null, true)
-  } else {
-    cb(null, false)
-  }
-}
-
 const upload = multer({
-  storage: storage,
+  storage,
   limits: {
-    fileSize: 1024 * 1024 * 5
+    fileSize: 1024 * 1024 * 5 // 5MB
   },
-  fileFilter: fileFilter
+  fileFilter (req, file, cb) {
+    const allowedMimes = [
+      'image/jpeg',
+      'image/png'
+    ]
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true)
+    } else {
+      cb(new Error('Tipo de arquivo inválido'), false)
+    }
+  }
 })
 
 module.exports = upload
